93bec8194240143ac0d7104ec7959e5e69b98fde
HashTH <bandit.dev@hotmail.com>
2023-03-16 11:13:17 +0700

    EP3 DONE

--- 3 my first game/3.py
+++ 3 my first game/3.py
@@ -5,9 +5,10 @@ game.init()
 
 #Background Color
 BG = (0,0,0)
+PLAYER = (50,0,0)
 
 #Frame per Second
-FPS = 60
+FPS = 30
 
 #resolution
 WIDTH = 800
@@ -16,11 +17,35 @@ HEIGHT = 700
 #set screen
 screen = game.display.set_mode((WIDTH, HEIGHT))
 #game name
-game.display.set_caption('Zero to Draw')
+game.display.set_caption('Cat The Dominator')
 #time elapsed
 clock = game.time.Clock()
 #situation
 running = True # True if running, False if not running
+
+class Player(game.sprite.Sprite):
+    #main function if running class
+    def __init__(self):
+        game.sprite.Sprite.__init__(self)
+        img = '3 my first game\cat.png'
+        self.image = game.image.load(img).convert_alpha()
+        #self.image = game.Surface((50,50))
+        #self.image.fill(PLAYER)
+
+        #create box
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2,HEIGHT/2)
+    
+    def update(self):
+        self.rect.y += 5
+        if self.rect.bottom > HEIGHT:
+            self.rect.y = 0
+
+#Create Sprite Group
+all_sprites = game.sprite.Group() # box of charactor
+player = Player() #create charactor
+all_sprites.add(player) #add charactor to box
+
 while running:
     #update
     clock.tick(FPS)
@@ -30,8 +55,13 @@ while running:
         if event.type == game.QUIT:
             running = False
 
+    all_sprites.update()
+
     #fill the screen
     screen.fill(BG)
+
+    #draw charactor to screen
+    all_sprites.draw(screen)
     
     #display 
     game.display.flip()
